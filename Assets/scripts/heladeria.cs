5d7b3a62f5405c98ab3b98ac0b7532dec67ab7c1
SamuelNakab <167198088+SamuelNakab@users.noreply.github.com>
2025-04-11 10:04:37 -0300

    Simulacro terminado

--- Assets/scripts/heladeria.cs
+++ Assets/scripts/heladeria.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class heladeria : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string codigoHelado;
+    public float cantidadDeGramosPedida;
+    float descuentoFrutilla = 0.1F;
+    float cantidadMaxima = 3000;
+    float cantidadMinima = 250;
+    float precioGramo = 1.25F;
+    float precio;
     void Start()
-    {
-        
+    { 
+        if(cantidadDeGramosPedida < 250 || cantidadDeGramosPedida > 3000)
+        {
+            Debug.Log("ERROR. Cantidad pedida no dentro de los parametros (250 - 3000)");
+        }
+        else
+        {
+            if (codigoHelado == "DDL")
+            {
+                precio = precioGramo * cantidadDeGramosPedida;
+                Debug.Log("El precio de tu helado de dulce de leche de " + cantidadDeGramosPedida + " gramos es de: " + precio + "$");
+                return;
+            }
+            else if(codigoHelado == "CHO")
+            {
+                precio = precioGramo * cantidadDeGramosPedida;
+                Debug.Log("El precio de tu helado de chocolate de " + cantidadDeGramosPedida + " gramos es de: " + precio + "$");
+                return;
+            }
+            else if(codigoHelado == "FRU")
+            {
+                precio = precioGramo * cantidadDeGramosPedida;
+                precio -= precio * descuentoFrutilla;
+                Debug.Log("El precio de tu helado de frutilla de " + cantidadDeGramosPedida + " gramos es de: " + precio + "$, estas usando un descuento del 10%");
+                return;
+            }
+            else
+            {
+                Debug.Log(" ERROR. Ingrese un codigo valido, (DDL, CHO, FRU)");
+            }
+         
+            
+        }
     }
 
     // Update is called once per frame
